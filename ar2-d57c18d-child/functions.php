b00d96e98ee04e9544b21d8c5f6bea741c4647cf
Alex Vallejo <alxvallejo@gmail.com>
2013-04-27 11:43:42 -0400

    require taxonomy functions

--- ar2-d57c18d-child/functions.php
+++ ar2-d57c18d-child/functions.php
@@ -1,5 +1,7 @@
 <?php
 // Edit
+require( __DIR__ . '/inc/seed-custom-taxonomies.php' );
+
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
 	register_post_type( 'board_of_director',
@@ -74,37 +76,4 @@ function create_post_type() {
 			'has_archive'   => true,
 		)
 	);
-}
-
-//hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_committee_taxonomies', 0 );
-
-//create two taxonomies, genres and writers for the post type "book"
-function create_committee_taxonomies()
-{
-  // Add new taxonomy, make it hierarchical (like categories)
-  $labels = array(
-    'name'                => _x( 'Committees', 'taxonomy general name' ),
-    'singular_name'       => _x( 'Committee', 'taxonomy singular name' ),
-    'search_items'        => __( 'Search Committees' ),
-    'all_items'           => __( 'All Committees' ),
-    'parent_item'         => __( 'Parent Committee' ),
-    'parent_item_colon'   => __( 'Parent Committee:' ),
-    'edit_item'           => __( 'Edit Committee' ),
-    'update_item'         => __( 'Update Committee' ),
-    'add_new_item'        => __( 'Add New Committee' ),
-    'new_item_name'       => __( 'New Committee Name' ),
-    'menu_name'           => __( 'Committees' )
-  );
-
-  $args = array(
-    'hierarchical'        => true,
-    'labels'              => $labels,
-    'show_ui'             => true,
-    'show_admin_column'   => true,
-    'query_var'           => true,
-    'rewrite'             => array( 'slug' => 'committees' )
-  );
-
-  register_taxonomy( 'committee', array( 'board_of_director' ), $args );
 }
