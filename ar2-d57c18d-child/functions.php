6a7975852b6c855196cc985dbb78a533ab273c3c
Alex Vallejo <alxvallejo@gmail.com>
2013-04-27 10:46:46 -0400

    Add functions

---
+++ ar2-d57c18d-child/functions.php
@@ -0,0 +1,14 @@
+<?php
+function create_post_type() {
+	register_post_type( 'acme_product',
+		array(
+			'labels' => array(
+				'name' => __( 'Products' ),
+				'singular_name' => __( 'Product' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'products'),
+		)
+	);
+}
